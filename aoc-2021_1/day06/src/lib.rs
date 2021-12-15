b15976f4a7a2c82e026afd7a9e49cbf33940d244
Morley, Jonathan <jmorley@cvent.com>
2021-12-15 10:59:52 -0500

    add aoc 2021

---
+++ aoc-2021_1/day06/src/lib.rs
@@ -0,0 +1,73 @@
+use anyhow::Result;
+use nalgebra::{ArrayStorage, SMatrix, SVector};
+
+fn generator(input: &str) -> [u64; 9] {
+    input
+        .split(",")
+        .map(str::parse)
+        .map(Result::unwrap)
+        .fold([0; 9], |mut acc, fish: usize| {
+            acc[fish] += 1;
+            acc
+        })
+}
+
+fn lanternfish(initial: Vec<u64>, day: u32) -> u64 {
+    let mut school = initial;
+
+    for _ in 0..day {
+        school.rotate_left(1);
+        school[6] += school[8];
+    }
+
+    school.into_iter().sum()
+}
+
+fn part1(input: &[u64; 9]) -> u64 {
+    lanternfish(Vec::from_iter(input.to_owned()), 80)
+}
+
+fn part2(input: &[u64; 9]) -> u64 {
+    lanternfish(Vec::from_iter(input.to_owned()), 256)
+}
+
+const LANTERNFISH_MATRIX: SMatrix<u64, 9, 9> = SMatrix::from_array_storage(ArrayStorage([
+    [0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0],
+]));
+
+fn part2_matrix(input: &[u64; 9]) -> u64 {
+    let mut m = LANTERNFISH_MATRIX;
+    (1..256).for_each(|_| m *= LANTERNFISH_MATRIX);
+
+    (m * SVector::from_column_slice(input)).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const SAMPLE: &str = "3,4,3,1,2";
+
+    #[test]
+    fn sample1() {
+        assert_eq!(part1(&generator(SAMPLE)), 5934);
+    }
+
+    #[test]
+    fn sample2() {
+        assert_eq!(part2(&generator(SAMPLE)), 26984457539);
+    }
+
+    #[test]
+    fn matrix2() {
+        assert_eq!(part2_matrix(&generator(SAMPLE)), 26984457539);
+    }
+}
---
+++ aoc-2021_1/day15/src/lib.rs
@@ -0,0 +1,119 @@
+use std::collections::HashMap;
+
+use petgraph::graphmap::DiGraphMap;
+use petgraph::algo::astar::astar;
+
+pub fn generator(input: &str) -> HashMap<(i32, i32), u64> {
+    input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line
+                .chars()
+                .enumerate()
+                .map(move |(x, c)| {
+                    ((x as i32, y as i32), c.to_digit(10).unwrap() as u64)
+                })
+        })
+        .collect()
+}
+
+pub fn part1(nodes: HashMap<(i32, i32), u64>) -> u64 {    
+    let mut graph = DiGraphMap::new();
+    
+    for (location, _) in &nodes {
+        for (dx, dy) in [(0, 1), (1, 0), (-1, 0), (0, -1)] {
+            let neighbour = (location.0 + dx, location.1 + dy);
+
+            if let Some(weight) = nodes.get(&neighbour) {
+                graph.add_edge(*location, neighbour, *weight);
+            }
+        }
+    }
+    
+    let max = graph.nodes().max().unwrap();
+
+    astar(
+        &graph,
+        (0, 0),
+        |finish| finish == max,
+        |(_, _, weight)| *weight,
+        |(x, y)| ((max.0 - x) + (max.1 - y)).abs() as u64 
+    ).unwrap().0
+}
+
+pub fn part2(nodes: HashMap<(i32, i32), u64>) -> u64 {
+    let mut nodes = nodes;
+
+    let min = nodes.keys().min().unwrap().to_owned();
+    let max = nodes.keys().max().unwrap().to_owned();
+    let (span_x, span_y) = (max.0 - min.0 + 1, max.1 - min.1 + 1);
+
+    // Stretch horizontally
+    for ((x, y), weight) in nodes.clone() {
+        for i in 1..5 {
+            nodes.insert(
+                (x + (span_x * i), y),
+                ((((weight as i32 + i) - 1) % 9) + 1) as u64
+            );
+        }
+    }
+
+    // Stretch vertically
+    for ((x, y), weight) in nodes.clone() {
+        for i in 1..5 {
+            nodes.insert(
+                (x, y + (span_y * i)),
+                ((((weight as i32 + i) - 1) % 9) + 1) as u64
+            );
+        }
+    }
+    
+    let mut graph = DiGraphMap::new();
+    
+    for (location, _) in &nodes {
+        for (dx, dy) in [(0, 1), (1, 0), (-1, 0), (0, -1)] {
+            let neighbour = (location.0 + dx, location.1 + dy);
+
+            if let Some(weight) = nodes.get(&neighbour) {
+                graph.add_edge(*location, neighbour, *weight);
+            }
+        }
+    }
+    
+    let max = graph.nodes().max().unwrap();    
+
+    astar(
+        &graph,
+        (0, 0),
+        |finish| finish == max,
+        |(_, _, weight)| *weight,
+        |(x, y)| ((max.0 - x) + (max.1 - y)).abs() as u64 
+    ).unwrap().0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const SAMPLE: &str = "1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581";
+
+    #[test]
+    fn sample1() {
+        assert_eq!(part1(generator(SAMPLE)), 40);
+    }
+
+    #[test]
+    fn sample2() {
+        assert_eq!(part2(generator(SAMPLE)), 315);
+    }
+}
