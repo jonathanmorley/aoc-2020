6dd123ad44174f4f8f915df51743f0343607f081
Jonathan Morley <jmorley@cvent.com>
2022-12-02 11:10:27 -0500

    simplify day 2

--- aoc-2022/src/day02.rs
+++ aoc-2022/src/day02.rs
@@ -1,41 +1,38 @@
-fn generator(input: &str) -> Vec<(&str, &str)> {
-    input.lines()
-        .map(str::split_whitespace)
-        .map(|mut s| (s.next().unwrap(), s.next().unwrap()))
-        .collect()
+pub fn generator(input: &str) -> Vec<&str> {
+    input.lines().collect()
 }
 
-pub fn part1(input: &str) -> usize {
-    generator(input)
+pub fn part1(input: &[&str]) -> usize {
+    input
         .into_iter()
-        .map(|event| match event {
-            ("A", "X") => 1 + 3,
-            ("A", "Y") => 2 + 6,
-            ("A", "Z") => 3 + 0,
-            ("B", "X") => 1 + 0,
-            ("B", "Y") => 2 + 3,
-            ("B", "Z") => 3 + 6,
-            ("C", "X") => 1 + 6,
-            ("C", "Y") => 2 + 0,
-            ("C", "Z") => 3 + 3,
+        .map(|event| match *event {
+            "A X" => 1 + 3,
+            "A Y" => 2 + 6,
+            "A Z" => 3 + 0,
+            "B X" => 1 + 0,
+            "B Y" => 2 + 3,
+            "B Z" => 3 + 6,
+            "C X" => 1 + 6,
+            "C Y" => 2 + 0,
+            "C Z" => 3 + 3,
             _ => unreachable!()
         })
         .sum::<usize>()
 }
 
-pub fn part2(input: &str) -> usize {
-    generator(input)
+pub fn part2(input: &[&str]) -> usize {
+    input
         .into_iter()
-        .map(|event| match event {
-            ("A", "X") => 3 + 0,
-            ("A", "Y") => 1 + 3,
-            ("A", "Z") => 2 + 6,
-            ("B", "X") => 1 + 0,
-            ("B", "Y") => 2 + 3,
-            ("B", "Z") => 3 + 6,
-            ("C", "X") => 2 + 0,
-            ("C", "Y") => 3 + 3,
-            ("C", "Z") => 1 + 6,
+        .map(|event| match *event {
+            "A X" => 3 + 0,
+            "A Y" => 1 + 3,
+            "A Z" => 2 + 6,
+            "B X" => 1 + 0,
+            "B Y" => 2 + 3,
+            "B Z" => 3 + 6,
+            "C X" => 2 + 0,
+            "C Y" => 3 + 3,
+            "C Z" => 1 + 6,
             _ => unreachable!()
         })
         .sum::<usize>()
@@ -51,11 +48,11 @@ C Z";
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(SAMPLE), 15);
+        assert_eq!(part1(&generator(SAMPLE)), 15);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(SAMPLE), 12);
+        assert_eq!(part2(&generator(SAMPLE)), 12);
     }
 }
--- aoc-2022/src/main.rs
+++ aoc-2022/src/main.rs
@@ -4,5 +4,5 @@ mod day02;
 aoc_main::main! {
   year 2022;
   day01             => part1, part2;
-  day02             => part1, part2;
+  day02 :generator  => part1, part2;
 }
